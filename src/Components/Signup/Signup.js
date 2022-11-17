b41995615abab1264fbf39a3dd1699a6872fb9eb
shafik720 <shafikrasel5@gmail.com>
2022-11-17 12:30:16 +0600

    sign up error does not show up if setCustomError is not set in error function

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -3,14 +3,15 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css';
 import googleLogo from '../../google.svg';
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init';
 
 const Signup = () => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const[confirmPassword, setConfirmPassword] = useState('');
-    const[customError, setCustomError] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [customError, setCustomError] = useState('');
+    let navigate = useNavigate();
 
     // using firebase hooks
     const [
@@ -18,69 +19,66 @@ const Signup = () => {
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth);
 
-    function handleEmail(e){
-        setEmail(e.target.value)    ;
+    function handleEmail(e) {
+        setEmail(e.target.value);
     }
-    function handlePassword(e){
+    function handlePassword(e) {
         setPassword(e.target.value);
     }
-    function handleConfirmPassword(e){
+    function handleConfirmPassword(e) {
         setConfirmPassword(e.target.value);
     }
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
-        if(password.length <6){
+        if (password.length < 6) {
             setCustomError('Password must be longer than 6 character !');
             return;
         }
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             setCustomError("Password didn't Matched");
             return;
         }
-        // if(error){
-        //     console.log(error);
-        //     setCustomError(error.message);
-        //     return;
-        // }
         setCustomError("");
         createUserWithEmailAndPassword(email, password);
     }
-    let navigate = useNavigate();
-    if(user){
+    if (error) {
+        console.log(error.message)
+        // setCustomError(error.message);
+    }else if (user) {
         navigate('/');
     }
     return (
         <div>
             <div className="container my-5">
                 <div className="row">
-                    <div className="col-lg-6 mx-auto">                        
+                    <div className="col-lg-6 mx-auto">
                         <div className=" form-div ">
-                        <h2 style={{color:'red'}}>Sign Up</h2>
-                        <hr style={{marginBottom:"40px"}} />
+                            <h2 style={{ color: 'red' }}>Sign Up</h2>
+                            <hr style={{ marginBottom: "40px" }} />
                             <form action="" onSubmit={handleSubmit}>
                                 <div onBlur={handleEmail} className="email-field">
                                     <p>Email :</p>
-                                    <input type="email" name="" id=""/>
+                                    <input type="email" name="" id="" />
                                 </div>
                                 <div onBlur={handlePassword} className="password-field">
                                     <p>Password :</p>
-                                    <input type="password" name="" id=""/>
+                                    <input type="password" name="" id="" />
                                 </div>
                                 <div onBlur={handleConfirmPassword} className="password-field">
                                     <p>Confirm Password :</p>
                                     <input type="password" name="" id="" />
-                                </div>                                
+                                </div>
                                 <div className="text-center error-area">
-                                    <p>{customError }</p>
+                                    <p>{customError}</p>
                                 </div>
                                 <button className="sign-up-button" type='submit'>Sign Up</button>
                                 <p className="signUpText">Already Have an Account ? <Link to="/login">Log in Here</Link> </p>
                                 <h4>Or</h4>
                                 <div draggable className="googleButton">
-                                    <img src={googleLogo} alt=""/>
+                                    <img src={googleLogo} alt="" />
                                     <h4>Sign in Using Google</h4>
                                 </div>
                             </form>
