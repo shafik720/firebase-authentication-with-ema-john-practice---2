9cf99cc5a97f699f8829b23d8e976204c8e8c52b
shafik720 <shafikrasel5@gmail.com>
2022-11-17 11:57:13 +0600

    installed firebase hook

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -3,12 +3,21 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.css';
 import googleLogo from '../../google.svg';
+import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 
 const Signup = () => {
     const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
     const[confirmPassword, setConfirmPassword] = useState('');
     const[customError, setCustomError] = useState('');
+
+    // using firebase hooks
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
 
     function handleEmail(e){
         setEmail(e.target.value)    ;
